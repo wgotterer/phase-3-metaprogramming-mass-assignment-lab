d6af7c9b1c0c3c3ad612b806103aa8342cb1be90
William Gotterer <william.gotterer@gmail.com>
2021-11-04 09:13:41 -0400

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -7,3 +7,10 @@ class Person
     end
   end
 end
+
+susan_attributes = { name: "Susan", height: "5'11\"", eye_color: "Green" }
+
+susan = Person.new(susan_attributes)
+susan.name      # => "Susan"
+susan.height    # => "5'11""
+susan.eye_color # => "Green"
